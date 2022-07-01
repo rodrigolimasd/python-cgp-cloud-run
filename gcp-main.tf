69f4cdc35b6ce60eefc7881dd13c64446ebeb521
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-06-30 21:31:53 -0300

    CGP Cloud Run

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,8 +1,4 @@
-resource "google_app_engine_application" "app" {
-  project     = "rds-labdevopscloud"
-  location_id = "us-central"
-}
-
+# Create Artifact Registry Repository for Docker containers
 resource "google_artifact_registry_repository" "python-gcp-cloud" {
   provider = google-beta
 
@@ -10,4 +6,56 @@ resource "google_artifact_registry_repository" "python-gcp-cloud" {
   repository_id = "pythongcpcloud"
   description = "Imagens Docker python gco cloud"
   format = "DOCKER"
+}
+
+# Deploy image to Cloud Run
+resource "google_cloud_run_service" "python-gcp-cloud" {
+  name     = "python-gcp-cloud"
+  location = "us-central"
+
+  metadata {
+    annotations = {
+      "run.googleapis.com/client-name" = "terraform",
+      "autoscaling.knative.dev/minScale" = "0"
+      "autoscaling.knative.dev/maxScale" = "1"
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  template {
+    spec {
+      containers {
+        # image_url = "us-centrl1-docker.pkg.dev/rds-labdevopscloud/pythongcpcloud/python-gcp-cloud"
+        image = local.image_url
+      }
+    }
+  }
+}
+
+# Create a policy that allows all users to invoke the API
+data "google_iam_policy" "noauth" {
+  provider = google-beta
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+# Apply the no-authentication policy to our Cloud Run Service.
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  provider = google-beta
+  location    = "us-central"
+  project     = "rds-labdevopscloud"
+  service     = google_cloud_run_service.python-gcp-cloud.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+output "cloud_run_instance_url" {
+  value = google_cloud_run_service.python-gcp-cloud.status.0.url
 }
